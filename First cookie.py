d64fd125460b520da543110360468e2f51fbdc2c
Ribeigu <gustavo.r.mota06@gmail.com>
2025-05-04 15:29:53 -0300

    Estudo sobre cookies

---
+++ First cookie.py
@@ -0,0 +1,17 @@
+from flask import Flask, make_response, request
+
+app = Flask(__name__)
+
+@app.route("/new_cookie")
+def new_cookie():
+    response = make_response()
+    response.setcookie("mycookie", "myvalue")
+    return response
+
+@app.route("/show_cookie")
+def show_cookie():
+    cookie_value = request.cookies.get("mycookie")
+    return cookie_value
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=9999)
---
+++ Trabalho cookie.py
@@ -0,0 +1,31 @@
+import os
+from http.cookies import SimpleCookie
+from datetime import datetime, timedelta
+import platform
+
+#Lê cookies existentes
+
+cookie = SimpleCookie(os.environ.get("HTTP_COOKIE"))
+
+#Define novos cookies
+
+lastsFor = (datetime.now(datetime.timezone.utc)+timedelta(days=7)).strftime("%a, %d-%b%Y &H:%M:%S GMT")
+cookie["user-session"]="usuario123456789"
+cookie["user-session"]["expires"]= lastsFor
+cookie["user-session"]["path"]= "/"
+
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+cookie["ultima_visita"]=timestamp
+cookie["referencia"]=os.environ.get("HTTP_REFERER", "desconhecido")
+cookie["navegador"]= os.environ.get("HTTP_USER_AGENT", "desconhecido")
+cookie["sistema"]= platform.system
+cookie["modo_escuro"]= "desconhecido"
+
+print("Content-Type: text/html")
+for morsel in cookie.values():
+    print(f"Set-Cookie:{morsel.OutputString()}")
+    print() #separa os headers do HTML
+
+#HTML da resposta
+
+print("<html><body><h1>Cookie de Gustavo Ribeiro definido com sucesso</h1></body></html>")
